ddf188030c3a623016f6f7ab14cedd451aeb5b38
SARA NORRIS <saramnorris@gatech.edu>
2016-09-24 15:26:52 -0400

    html changes

--- flaskr.py
+++ flaskr.py
@@ -4,8 +4,6 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 
 from expedia import getFlightInfo
-
-# create our little application :)
 app = Flask(__name__)
 
 @app.route('/')
